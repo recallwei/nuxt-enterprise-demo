bba4d153996af718a47d0ff077453904481fabe5
Bruce <recall4056@gmail.com>
2023-09-05 10:22:29 +0800

    chore: update

--- components/BackToTop.vue
+++ components/BackToTop.vue
@@ -18,9 +18,11 @@ onMounted(() => {
 
 <template>
   <div
-    class="fixed bottom-8 right-4 flex cursor-pointer items-center justify-center rounded-full bg-purple-300 p-2.5 shadow-lg drop-shadow-lg transition-all duration-500 hover:opacity-90 active:opacity-75 sm:bottom-16 sm:right-12"
+    class="fixed bottom-8 right-4 flex items-center justify-center rounded-full bg-purple-300 p-2.5 shadow-lg drop-shadow-lg transition-all duration-500 sm:bottom-16 sm:right-12"
     :class="[
-      showBtn ? 'translate-y-0 opacity-100' : '-translate-y-80 opacity-0'
+      showBtn
+        ? 'translate-y-0 cursor-pointer opacity-100 hover:opacity-90 active:opacity-75'
+        : '-translate-y-80 opacity-0'
     ]"
     @click="scrollToTop"
   >
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' }
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+      link: [{ rel: 'icon', href: '/favicon.ico', sizes: 'any' }]
     }
   },
   ssr: true,
