0afd3be625df84a2076874384e4bea7da8f2f5bd
Bruce <recall4056@gmail.com>
2023-09-06 01:46:41 +0800

    chore: update

--- components/PTopBanner.vue
+++ components/PTopBanner.vue
@@ -24,7 +24,7 @@ const textMotion = ref({
     opacity: 0,
     y: 100
   },
-  visible: {
+  enter: {
     opacity: 1,
     y: 0,
     transition: {
@@ -33,8 +33,7 @@ const textMotion = ref({
       damping: 50,
       mass: 1
     }
-  },
-  delay: 50
+  }
 })
 </script>
 
